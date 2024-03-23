6ba454ea5b55cf85b80d18c88b72c974c3077d0b
dhananjaysingh10 <128625326+dhananjaysingh10@users.noreply.github.com>
2024-03-23 22:19:46 +0530

    Update index.js
    Handle the case when all cells are filled but no one won

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ let isGameOver = 0;
 function changeTurn(){
     return currMove === 'X'?'O':'X';
 }
+function allBoxesFilled() {
+    let boxtext = document.getElementsByClassName('boxText');
+    for (let i = 0; i < boxtext.length; i++) {
+        if (boxtext[i].innerText === '') {
+            return false; // If any box is empty, return false
+        }
+    }
+    return true; // All boxes are filled
+}
 
 //Check win after each move 
 function checkWin(){
@@ -27,76 +36,51 @@ function checkWin(){
             document.querySelector(".info").innerText = boxtext[e[0]].innerText + ' WON';
             isGameOver = 1;
             gameOver.play();
+            return;
             // reset();
         }
     }
 
     )
+    // If no one wins and all boxes are filled, it's a draw
+    if (allBoxesFilled()) {
+        document.querySelector(".info").innerText = 'Draw! No one wins.';
+        isGameOver = 1;
+        return;
+    }
+
 }
-let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element=>{
-                    let boxtext = element.querySelector(".boxText");
-                    element.addEventListener('click', ()=>{
-                        if(boxtext.innerHTML==='' && isGameOver!==1){
-                            boxtext.innerHTML=currMove;
-                            currMove = changeTurn();
-                            validMoveSound.play();
-                            checkWin();
-                            if(!isGameOver){
-                            document.querySelector(".info").innerHTML=`${currMove}'s turn`
-                            }
-                        }else{
-                            inValidMoveSound.play();
-                        }
-                        // changeTurn();
-                    } )
-})
 
 // Reset
-let rstBtn = document.getElementById("reset");
-rstBtn.addEventListener('click', 
-()=>{
-    let boxes = document.getElementsByClassName("boxText");
-    let i = 0;
-    Array.from(boxes).forEach(element=>{
-                        boxes[i].innerHTML='';
-                        i++;
-                        isGameOver = 0;
-                        resetSound.play();
-                        currMove = 'X';
-                        if(!isGameOver){
-                            document.querySelector(".info").innerHTML=`${currMove}'s turn`
-                            }
-                        })
+function resetGame() {
+    let boxTexts = document.getElementsByClassName("boxText"); // Change variable name to avoid conflict
+    for (let i = 0; i < boxTexts.length; i++) {
+        boxTexts[i].innerHTML = '';
+    }
+    isGameOver = 0;
+    resetSound.play();
+    currMove = 'X';
+    document.querySelector(".info").innerHTML = `${currMove}'s turn`;
 }
-)
-function allFilled(){
-    let boxes = document.getElementsByClassName("box");
-    Array.from(boxes).forEach(element=>{
-                    let boxtext = element.querySelector(".boxText");
-                    element.addEventListener('click', ()=>{
-                        if(boxtext.innerHTML==='' && isGameOver!==1){
-                            boxtext.innerHTML=currMove;
-                            currMove = changeTurn();
-                            validMoveSound.play();
-                            checkWin();
-                            if(!isGameOver){
-                            document.querySelector(".info").innerHTML=`${currMove}'s turn`
-                            }
-                        }else{
-                            inValidMoveSound.play();
-                        }
-                        // changeTurn();
-                    } )
-})
-}
-// function reset(){
-//     let boxes = document.getElementsByClassName("boxText");
-//     let i = 0;
-//     Array.from(boxes).forEach(element=>{
-//                         boxes[i].innerHTML='';
-//                         i++;
-//                         isGameOver = 0;
-//         })
 
-// }
+// Event listeners
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector(".boxText");
+    element.addEventListener('click', () => {
+        if (boxtext.innerHTML === '' && isGameOver !== 1) {
+            boxtext.innerHTML = currMove;
+            currMove = changeTurn();
+            validMoveSound.play();
+            checkWin();
+            if (!isGameOver) {
+                document.querySelector(".info").innerHTML = `${currMove}'s turn`;
+            }
+        } else {
+            inValidMoveSound.play();
+        }
+    });
+});
+// Reset button event listener
+let rstBtn = document.getElementById("reset");
+rstBtn.addEventListener('click', resetGame);
